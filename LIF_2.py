6e029cc1d5791ae9aaaae431a8eb9c205191879e
Benano <ben.v.h@hotmail.com>
2021-02-05 16:01:54 +0100

    Finished PyNN and Own implementation

--- LIF_2.py
+++ LIF_2.py
@@ -69,13 +69,27 @@ def LIF_CUBA(spikes=np.zeros(1),duration=100,step=0.1,cm=1,tau_m=20,I_ext=0,
         
         u_vec.append(u)
         I_vec.append(I_syn)
+    
+    plt.figure()
+    plt.title("Voltage")
+    plt.plot(u_vec)
+
+    plt.figure()
+    plt.title("Current from Synapse")
+    plt.plot(I_vec)
+
+    plt.figure()
+    plt.title("Spikes")
+    plt.plot(s_vec)
+
+    plt.show()
 
     return I_vec, u_vec, s_vec
     
 # %% LIF COBA
 
-def LIF_COBA(spikes_e=np.zeros(1),spikes_i=np.zeros(1),duration=100,step=0.1,cm=1,tau_m=20,
-            E_rest=-65,E_rev_e=0,E_rev_i=-70,tau_syn_e=20,tau_syn_i=20,v_reset=-65,tau_refrac=0.1,v_thresh=-50):
+def LIF_COBA(spikes_e=np.zeros(1),spikes_i=np.zeros(1),duration=100,step=0.1,cm=1,tau_m=20,w=0.016,
+            E_rest=-65,E_rev_e=0,E_rev_i=-70,tau_syn_e=5,tau_syn_i=5,v_reset=-65,tau_refrac=0.1,v_thresh=-50):
 
     ''' 
     Simulation of a LIF COBA neuron.
@@ -109,7 +123,7 @@ def LIF_COBA(spikes_e=np.zeros(1),spikes_i=np.zeros(1),duration=100,step=0.1,cm=
         
         if np.any(spikes_i):
             if spikes_i[t]:
-                g_syn_i += 0.05
+                g_syn_i += 1
 
         # Refractory Period
         if 1 in s_vec[-(int(tau_refrac/step)):]:
@@ -117,7 +131,7 @@ def LIF_COBA(spikes_e=np.zeros(1),spikes_i=np.zeros(1),duration=100,step=0.1,cm=
 
         # Changing Voltage
         else:
-            du = (((g1*(E_rest - u)) + g_syn_e*(E_rev_e-u) + g_syn_i*(E_rev_i-u)) / cm)*step
+            du = (((g1*(E_rest - u)) + g_syn_e*(E_rev_e-u) + w*g_syn_i*(E_rev_i-u)) / cm)*step
             u += du
         
         # Spiking
@@ -130,73 +144,101 @@ def LIF_COBA(spikes_e=np.zeros(1),spikes_i=np.zeros(1),duration=100,step=0.1,cm=
         u_vec.append(u)
         ge_vec.append(g_syn_e)
         gi_vec.append(g_syn_i)
+    
+    plt.figure()
+    plt.title("g_e")
+    plt.plot(ge_vec)
+
+    plt.figure()
+    plt.title("g_i")
+    plt.plot(gi_vec)
+
+    plt.figure()
+    plt.title("Voltage")
+    plt.plot(u_vec)
+
+    plt.figure()
+    plt.title("spikes")
+    plt.plot(s_vec)
+
+    plt.show()
 
     return ge_vec, gi_vec, u_vec, s_vec
+
     
-# %% Simulation LIF COBA
+# %% Simulation
 
 spikes_e = make_spikes(20,22,3)
 spikes_i = make_spikes(70,72,3)
-ge, gi, u, sp  = LIF_COBA(spikes_e=spikes_e,tau_syn_e=5)
 
+# COBA
+ge_vec, gi_vec, u_vec, s_vec = LIF_COBA(spikes_e=spikes_e,tau_syn_e=5)
 
-#%% # Plotting 
-plt.figure()
-plt.title("g_e")
-plt.plot(ge)
-
-plt.figure()
-plt.title("g_i")
-plt.plot(gi)
-
-plt.figure()
-plt.title("voltage")
-plt.plot(u)
-
-plt.figure()
-plt.title("spikes")
-plt.plot(sp)
+# CUBA
+I_vec, u_vec, s_vec  = LIF_CUBA(spikes=spikes_e)
 
 
 # %% PyNN CUBA
-
 # Setup
 sim.setup(timestep=0.1, min_delay=0.1, max_delay=10.0)
-IF_sim = sim.Population(1, sim.IF_curr_exp(i_offset=1.0), label="IF_curr_exp")
+IF_sim = sim.Population(1, sim.IF_curr_exp(), label="IF_curr_exp")
 IF_sim.record('v')
 
+spike_times = [20]
+# spike_times = np.arange(40,60,5)
+spike_input = sim.Population(1,sim.SpikeSourceArray(spike_times=spike_times),label="Input spikes")
+
+# Connections
+w = 1
+connections = sim.Projection(spike_input, IF_sim,
+                            connector=sim.AllToAllConnector(),
+                            synapse_type=sim.StaticSynapse(weight=w,delay=0.1),
+                            receptor_type="excitatory")
+
 # Running simulation in MS
+IF_sim.record('v')
 sim.run(100.0)
 
 # Data 
 v_data = IF_sim.get_data()   
 data = IF_sim.get_data().segments[0]
-v = data.filter(name="v")[0]
+v_cuba = data.filter(name="v")[0]
 
 # Plotting
-plt.plot(v)
+plt.plot(v_cuba)
 
 # End
 sim.end()
 
-# %% PyNN COBA
+# PyNN COBA
 
 # Setup
 sim.setup(timestep=0.1, min_delay=0.1, max_delay=10.0)
 IF_sim = sim.Population(1, sim.IF_cond_exp(), label="IF_curr_exp")
 IF_sim.record('v')
 
+w = 0.016
+spike_times = [20]
+# spike_times = np.arange(40,60,5)
+spike_input = sim.Population(1,sim.SpikeSourceArray(spike_times=spike_times),label="Input spikes")
+
+
+connections = sim.Projection(spike_input, IF_sim,
+                            connector=sim.AllToAllConnector(),
+                            synapse_type=sim.StaticSynapse(weight=w,delay=0.1),
+                            receptor_type="excitatory")
+
 # Running simulation in MS
 sim.run(100.0)
 
 # Data 
 v_data = IF_sim.get_data()   
 data = IF_sim.get_data().segments[0]
-v = data.filter(name="v")[0]
+v_coba = data.filter(name="v")[0]
 
 # Plotting
-plt.plot(v)
+plt.plot(v_coba)
 
 # End
 sim.end()
-# %%
+
