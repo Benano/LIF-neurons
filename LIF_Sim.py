bc36bf1165eeb2f996d42842d78e199ab551fdb9
Benano <ben.v.h@hotmail.com>
2021-02-14 23:21:45 +0100

    Groundwork for the spiking input implementation

--- LIF_Sim.py
+++ LIF_Sim.py
@@ -6,29 +6,37 @@ class sim:
     def __init__(self,increment): #input & output
         self.increment = increment
 
+    neuron_labels = []
     neurons = []
     synapses = []
 
-    def add_neuron(self,neuron):
+    def add_neuron(self,neuron,label=None):
         self.neurons.append(neuron)
+        self.synapses.append([])
 
-    def add_synapse(self,synapse):
-        self.synapses.append(synapse)
+        if label:
+            self.neuron_labels = label
+        else:
+            self.neuron_labels = "neuron_" + str(len(self.neuron))
+
+    def add_synapse(self,pre,post):
+        self.synapses[post].append(pre)
+    
+    def current_sim(self):
+        print(self.neuron_labels)
         
     def run(self,duration):
         # MS conversion
         steps = int(duration/self.increment)
         for i in range(steps):
-            for neuron in sim.neurons:
-                neuron.update(self.increment)
-                # For synapse in synapses
-                   
-class synapse():
-    def __init__(self,pre,post):
-        self.pre = pre
-        self.post = post
+            for idx,neuron in enumerate(sim.neurons):
+                neuron.update(self.increment,spike_input)
 
-        
+                if i:
+                    if synapses[i]:
+                        for s in range(synapses):
+
+            
 
 class neuron():
     def __init__(self,tau_m=20,cm=1,tau_syn=5,E_rest=-65,v_reset=-65,tau_refrac=0.1,v_thresh=-50): # Attributes shared by all
@@ -56,7 +64,7 @@ class lif_coba(neuron):
         self.u_ts = []
         self.s_ts = []
 
-    def update(self,increment):
+    def update(self,increment,spike_input=None):
         # Voltage
         du = (((self.g1*(self.E_rest - self.u)) + self.g_syn_e*(self.E_rev_e-self.u) + self.g_syn_i*(self.E_rev_i-self.u)) / self.cm)* increment
         self.u += du
@@ -83,7 +91,7 @@ class lif_cuba(neuron):
         self.u_ts = []
         self.s_ts = []
 
-    def update(self,increment):
+    def update(self,increment,spike_input=None):
         # Voltage
         du = (((self.g1*(self.E_rest - self.u)) + self.I_ext) / self.cm)* increment
         self.u += du
@@ -101,11 +109,31 @@ class lif_cuba(neuron):
         
         self.u_ts.append(self.u)
 
+class spike_array(sim):
+    def __init__(self,spike_times):
+        super().__init__(increment)
+
+        self.spike_times = np.array(spike_times)/self.increment
+
+
+
+
+
 
 # %% 
 sim1 = sim(0.1)
 n1 = lif_cuba(I_ext=1)
-sim1.add_neuron(n1)
+n2 = lif_cuba(I_ext=1.5)
+
+sim1.add_neuron(n1,"Lif_Coba_1")
+sim1.add_neuron(n2,"Lif_Coba_2")
+sim1.add_synapse(0,1)
+sim1.current_sim()
+
 sim1.run(100)
 
 plt.plot(n1.u_ts)
+
+print(sim.synapses)
+
+
